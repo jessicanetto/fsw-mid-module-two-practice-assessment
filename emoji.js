ab0d87793e29e339a93e1a1c7a0149f13082a1e7
jessicanetto <75263227+jessicanetto@users.noreply.github.com>
2021-02-27 12:06:54 -0500

    emoji story making stories

--- emoji.js
+++ emoji.js
@@ -1,1 +1,16 @@
-console.log("hello");
+const emojis = ["😀", "😆", "😅", "🤣", "🙃", "😍", "😡", "🥶", "😱", "👻", "👾", "😻", "💋", "👅", "🧵", "👠", "🥾", "👑", "🎓", "🐔", "🐧", "🦄", "🦇", "🌲", "🎄", "🌊", "🍓", "🍍", "🥯", "🌭", "🍕", "🍣", "🥡", "🤸", "🎯", "🧨", "🔮", "💉", "🦠", "❤️", "🧡", "💛", "💚", "💙", "💜", "☢️", "🚫", "💯", "🔱", "🎊"];
+
+let emojiButton = document.getElementById("generate-story-btn");
+let storyElement = document.getElementById("story");
+emojiButton.addEventListener("click", () => {
+  //receive number input
+  const storyLength = document.getElementById("story-length-input").value;
+  //randomly selects (inputted number) of emojis
+  const shuffled = emojis.sort(() => 0.5 - Math.random());
+  let selectedEmojis = shuffled.slice(0, storyLength);
+  //displays randomly selected emojis in story section
+  let emojiStoryElement = document.createElement("p");
+  //made an element, now we set its textcontent
+  emojiStoryElement.textContent = selectedEmojis;
+  storyElement.appendChild(emojiStoryElement);
+});
